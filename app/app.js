577251e866ef5151c77afaa5aa8854630194908b
SviataslauS <Sviataslau.salamennik@logicsoftware.net>
2019-07-07 17:44:02 +0300

    added swagger

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
+var opn = require('opn');
 var express = require('express');
 
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('It\'s a Blog, mothefucker!');
+    res.send('It\'s a Blog, motherfucker!');
 });
 
 var http = require('http');
@@ -12,3 +13,4 @@ var server = http.createServer(app);
 var port = process.env.PORT || '3000';
 server.listen(port);
 
+opn('http://localhost:3000');
